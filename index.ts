a195c4e835e80e46d198edf82d9cd9bb496a1811
popalnicolas <popal.nicolas@gmail.com>
2022-10-01 12:30:47 +0200

    creating fargate service

--- index.ts
+++ index.ts
@@ -11,3 +11,23 @@ const repository = new awsx.ecr.Repository(`${projectName}-repo-${getStack()}`);
 const dockerImage = repository.buildAndPushImage("./app");
 
 const web = new awsx.elasticloadbalancingv2.NetworkListener(`${projectName}-lb-${getStack()}`, { port: 80 });
+const service = new awsx.ecs.FargateService(`${projectName}-td-${getStack()}`, {
+    name: `${projectName}-service-${getStack()}`,
+    taskDefinitionArgs: {
+        containers: {
+            reactApp: {
+                image: dockerImage/*awsx.ecs.Image.fromPath(`${projectName}-docker-${getStack()}`, "./app")*/,
+                memory: 512,
+                portMappings: [web],
+                environment: [
+                    {
+                        name: "nginx_pass",
+                        value: password
+                    }
+                ],
+            },
+        },
+    },
+});
+
+export const url = web.endpoint.hostname;
